c1bca9a63a17d715eae945cdafcab84e07ddbdf6
Jade Bautista <edaj.bautista@gmail.com>
2017-08-30 15:05:36 +0800

    Form submit test event

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -19,4 +19,20 @@ describe('Comment Box', () => {
     it('has a button', () => {
         expect(component.find('button')).to.exist;
     });
+
+    describe('entering sometext', () => {
+        beforeEach(() => {
+            component.find('textarea').simulate('change','new comment');
+        });
+
+        it('shows that text in the textarea', () => {
+            expect(component.find('textarea')).to.have.value('new comment');
+        });
+
+        it('when submit, clear input', () => {
+            component.simulate('submit');
+            expect(component.find('textarea')).to.have.value('');
+
+        });
+    });
 });
